5ef584f8e88f12de100e2ca5954033343db1ca73
Bektursun <samatuulu11@gmail.com>
2019-10-19 12:47:34 +0600

    Initial and Created models Poll and Choice. Also, started project from beginning, run projects, created needed files for the project. Created superuser.

---
+++ source/questiona/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Poll(models.Model):
+    question = models.TextField(max_length=150, null=False, blank=False, verbose_name='Question')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+
+class Choice(models.Model):
+    answer = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Answer')
+    poll = models.ForeignKey('questiona.Poll', related_name='poll_choce', on_delete=models.CASCADE, verbose_name='Poll')
