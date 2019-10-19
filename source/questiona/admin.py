0c29f2a33fe59d9da9ef588fccd95e9e48190a79
Bektursun <samatuulu11@gmail.com>
2019-10-19 13:17:29 +0600

    Edited some part of the project that related to first step. Added dump.json file

--- source/questiona/admin.py
+++ source/questiona/admin.py
@@ -3,5 +3,20 @@ from django.contrib import admin
 from .models import Poll, Choice
 
 
-admin.site.register(Poll)
-admin.site.register(Choice)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created_at']
+    list_filter = ['question']
+    list_display_links = ['pk', 'question']
+    search_fields = ['question']
+    fields = ['question']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'answer']
+    list_filter = ['answer']
+    list_display_links = ['pk', 'answer']
+    search_fields = ['answer']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- source/questiona/models.py
+++ source/questiona/models.py
@@ -5,7 +5,13 @@ class Poll(models.Model):
     question = models.TextField(max_length=150, null=False, blank=False, verbose_name='Question')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
 
+    def __str__(self):
+        return self.question
+
 
 class Choice(models.Model):
     answer = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Answer')
     poll = models.ForeignKey('questiona.Poll', related_name='poll_choce', on_delete=models.CASCADE, verbose_name='Poll')
+
+    def __str__(self):
+        return self.answer
